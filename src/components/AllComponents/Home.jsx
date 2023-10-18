72fc655e74ef7e7f8fa09a949ed84e23f74b1ffb
Joshua Okorie <joshuaokorie008@gmail.com>
2023-10-18 11:05:04 +0100

    completed the wrapper section

--- src/components/AllComponents/Home.jsx
+++ src/components/AllComponents/Home.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import NavBar from "../NavBar/NavBar";
 import Header from "../Header/Header";
+import WrapperContainer from "../wrapperContainer/WrapperContainer";
 
 const Home = () => {
   return (
     <div>
-      <NavBar />
-      <Header />
+      <WrapperContainer navHead>
+        <NavBar />
+        <Header />
+      </WrapperContainer>
+      <div className=" bg-gray-100 w-full">
+        <WrapperContainer></WrapperContainer>
+      </div>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,20 @@ module.exports = withMT({
         royalBlue: "#4169e1",
         darkBlue: "#0d1117",
         dark: "#0d1117",
-      }
+      },
+        backgroundImage: {
+      wrapperBg: "url('assets/images/wrapperBackground.svg')",
+      wrapperDark: "url('assets/images/wrapperDark.svg')",
+    },
+       container: {
+      screens: {
+        sm: "640px",
+        md: "767px",
+        lg: "1140px",
+        xl: "1400px",
+        "2xl": "1800px",
+      },
+    },
     },
   },
   plugins: [],
